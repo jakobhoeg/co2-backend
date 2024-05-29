3896c08ddcf3500d5adecffd57ac9d0479a50c8e
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-05-29 10:48:07 +0200

    fix: easier switch between dev and prod env (for testing locally)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { routes } from "./routes/routes.js";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 // Ensure that the port is always a number by parsing it
 const port = parseInt(process.env.PORT);
 
-const server = routes.listen(port, "0.0.0.0", () => {
-  console.log(`This server is listening at http://0.0.0.0:${port}`);
-});
+if (process.env.NODE_ENV === "development") {
+  routes.listen(port, "localhost", () => {
+    console.log(`This server is listening at http://localhost:${port}`);
+  });
+} else {
+  routes.listen(port, "0.0.0.0", () => {
+    console.log(`This server is listening at http://0.0.0.0:${port}`);
+  });
+}
